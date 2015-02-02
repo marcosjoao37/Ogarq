961eb708f123ac9c901b0c5c250e319ede0c14ab
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-02-02 21:10:54 -0200

    removido tratamento de pastas

---
+++ Scripts_/OgarqSetup.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+
+python3.4 src/main.py
--- src/funcoes/DicionarioDeArquivos.py
+++ src/funcoes/DicionarioDeArquivos.py
@@ -3,8 +3,7 @@ __author__ = 'João Marcos Silva e Araújo'
 
 import glob
 
-from funcoes.TratamentoDePastas import TratamentoDePastas
-#from funcoes.MoverECopiar import MoverECopiar
+
 
 """
 Dicionário com o nome de todas as extensões de dda.
@@ -20,15 +19,5 @@ class DicionarioDeArquivos:
                          "Músicas_" : 'mp3,wav,flac,aac',
                          "Vídeos_" : 'wmv,mp4,mkv,rmvb,flv,3gp,avi,mov'
         }
-"""
-
-    A principal tarefa desta função {leituraEsc()} é a de criar listas com os nomes dos dda da pasta para qual o
-    programa foi apontado. Por exemplo, ele gerará a lista self.listFoto com o nome do diretório completo das imagens
-    que estão na pasta. Ex: Na pasta "/home/usuario/Downloads", existe um arquivo chamado "foto.jpg", logo, gerará uma
-    trupla na lista chamada "/home/usuario/Downloads/foto.jpg". Os parâmetros 'esc' e 'dir' são referentes à outra
-    classe, 'esc' referente a ESCOLHA de COPIAR ou MOVER or dda; 'dir' referente ao DIRETÓRIO onde estarão os
-    dda, que será capturado na área de texto da interface TK, presente no arquivo io/InterfaceOgarq.py.
-    """
 
 
-tdp = TratamentoDePastas()
